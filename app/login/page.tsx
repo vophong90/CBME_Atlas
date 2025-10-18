af875be0901f7a09ef0141f6ec0f953ec95c79f4
vophong90 <141991252+vophong90@users.noreply.github.com>
2025-10-18 21:39:04 +0700

    Update page.tsx

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,4 +1,6 @@
 'use client'
+export const dynamic = 'force-dynamic'
+export const revalidate = 0
 import { useEffect, useState } from 'react'
 import { useRouter, useSearchParams } from 'next/navigation'
 import { supabase } from '../../lib/supabase'
