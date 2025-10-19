913b68ebf84d150b6fa99863de3624d9c84e16b9
vophong90 <141991252+vophong90@users.noreply.github.com>
2025-10-19 21:59:45 +0700

    Create react-cytoscapejs.d.ts

---
+++ types/react-cytoscapejs.d.ts
@@ -0,0 +1,6 @@
+// types/react-cytoscapejs.d.ts
+declare module 'react-cytoscapejs' {
+  import * as React from 'react';
+  const ReactCytoscape: React.ComponentType<any>;
+  export default ReactCytoscape;
+}
